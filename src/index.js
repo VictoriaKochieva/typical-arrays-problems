95b00975247ee2a0d7c8d883bc3dfec3592a7a09
VictoriaKochieva <kochievavictoria@gmail.com>
2021-03-14 12:35:32 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+  if ( Array.isArray(array) &&array.length > 0) {array[0]} else return 0;
+    let minNumber = Math.min.apply(null, array)
+    for ( i = 0; i< array.length; i++) {
+      if ( array[i] < minNumber) {
+        minNumber = array[i]
+      } 
+    }
+   return minNumber
+  }
+  
+  exports.max = function max (array) {
+    if ( Array.isArray(array) &&array.length > 0) {array[0]} else return 0;  
+    let maxNumber = Math.max.apply(null, array)
+    for ( i = 0; i< array.length; i++) {
+      if ( array[i] > maxNumber) {
+        maxNumber = array[i]
+      } 
+    }
+   return maxNumber
+  }
+  
+  exports.avg = function avg (array) {
+    if ( Array.isArray(array) &&array.length > 0) {array[0]} else return 0; 
+    let sum = array.reduce(function(a, b) {return a+b})
+   
+    for ( i = 0; i< array.length; i++) {
+     let length = array.length;  
+     return result = sum / length;
+    }
+  }
+  
